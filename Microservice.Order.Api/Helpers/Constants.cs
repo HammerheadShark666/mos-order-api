f957865dfe9dd296005ccc975b7ab39b494bdd1a
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2024-09-15 17:10:28 +0100

    Setup Sql Server managed identity

--- Microservice.Order.Api/Helpers/Constants.cs
+++ Microservice.Order.Api/Helpers/Constants.cs
@@ -13,7 +13,13 @@ public class Constants
     public const string AzureServiceBusConnectionManagedIdentity = "ServiceBusConnection__fullyQualifiedNamespace";
     public const string AzureServiceBusQueueOrderCompleted = "AZURE_SERVICE_BUS_QUEUE_ORDER_COMPLETED";
 
-    public const string DatabaseConnectionString = "SQLAZURECONNSTR_ORDER";
+    public const string AzureUserAssignedManagedIdentityClientId = "AZURE_USER_ASSIGNED_MANAGED_IDENTITY_CLIENT_ID";
+    public const string AzureDatabaseConnectionString = "AZURE_MANAGED_IDENTITY_SQL_CONNECTION";
+
+    public const string AzureLocalDevelopmentClientId = "AZURE_LOCAL_DEVELOPMENT_CLIENT_ID";
+    public const string AzureLocalDevelopmentClientSecret = "AZURE_LOCAL_DEVELOPMENT_CLIENT_SECRET";
+    public const string AzureLocalDevelopmentTenantId = "AZURE_LOCAL_DEVELOPMENT_TENANT_ID";
+    public const string LocalDatabaseConnectionString = "LOCAL_CONNECTION";
 
     public const string DateFormat_ddMMyyyy = "dd/MM/yyyy";
 }
